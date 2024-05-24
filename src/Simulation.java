788a866752e99e397f1efe27b84846ffbef0b3b8
czewski <wiktorczewski@gmail.com>
2024-05-24 22:56:48 +0200

    struktura klas 3

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,4 +1,23 @@
 public class Simulation {
     double time;
 
+    public void show_crop_area(double area){
+
+    }
+
+    public void show_water_demand(double demand){
+
+    }
+
+    public void show_food_demand(double demand){
+
+    }
+
+    public void time_progress(double time){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
